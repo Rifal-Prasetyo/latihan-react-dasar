3f067e916c188a98bcc429c240a4e00b0b659ae0
Rifal-Prasetyo <hrdsdms765@gmail.com>
2024-07-07 21:53:44 +0700

    Eps 9-2 - Interaksi dengan Back-End, Pemanggilan API (GET) Part 2

--- src/container/BlogPost/BlogSpot.jsx
+++ src/container/BlogPost/BlogSpot.jsx
@@ -1,11 +1,39 @@
 import React, { Component, Fragment } from "react";
 import Post from "../../components/Post/Post";
+import axios from "axios";
 class BlogSpot extends Component {
+
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         this.setState({
+        //             posts: data
+        //         });
+        //     })
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                this.setState({
+                    posts: res.data
+                });
+            })
+
+    }
     render() {
         return (
             <Fragment>
                 <h1>Blog Spot</h1>
-                <Post />
+                {
+                    this.state.posts.map(post => {
+                        return (
+
+                            <Post key={post.id} title={post.title} desc={post.body} />
+                        )
+                    })
+                }
             </Fragment>
         )
     }
