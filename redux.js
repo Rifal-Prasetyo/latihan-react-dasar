f6ec14fa31a7097885c7f68aedb8fd6ae1f7acd3
Rifal-Prasetyo <hrdsdms765@gmail.com>
2024-08-31 11:45:45 +0700

    Memahami Konsep Redux Part 2 - #16 ReactJS Tutorial

--- redux.js
+++ redux.js
@@ -1,5 +1,5 @@
 const redux = require('redux');
-const createStore = redux.createStore;
+const createStore = redux.legacy_createStore;
 
 const initalState = {
     value: 0,
@@ -8,12 +8,32 @@ const initalState = {
 
 // Reducer (fungsi bahwa Store bisa diupdate memodifikasi hanya dari Reducer) / Tasklist
 const rootReducer = (state = initalState, action) => {
-    return state
+    
+    switch(action.type) {
+        case "ADD_AGE":
+            return {
+                ...state,
+                age: state.age + 1
+            }
+        case "CHANGE_VALUE":
+            return {
+                ...state,
+                value: state.value + action.newValue
+            }
+        default: 
+            return state
+    }
+
 }
 
 // Store (wadah besar yang menyimpan value state secata global)
 const store = createStore(rootReducer);
 console.log(store.getState()) // state global
+//Subscription(Proses Pemanggilan Store ketika terjadi perubahan}
+store.subscribe(() => console.log("store change", store.getState()))
 //Dispatch / Action (Proses pemanggilan/ intruksi spesifik mengubah state)
+store.dispatch({type: 'ADD_AGE'});
+console.log(store.getState()) // state global
+store.dispatch({type: "CHANGE_VALUE", newValue: 99});
+console.log(store.getState()) // state global
 
-//Subscription(Proses Pemanggilan Store yang diperlukan)
