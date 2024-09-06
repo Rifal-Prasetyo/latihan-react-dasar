9748e3b9c5f755de554810b37b50868c22cf8a31
Rifal-Prasetyo <hrdsdms765@gmail.com>
2024-09-06 22:40:35 +0700

    React Hooks Side Effect atau LifeCycle Hooks Part 2 28 ReactJS Tutorial Indonesia

--- src/container/pages/Hooks/Hooks.jsx
+++ src/container/pages/Hooks/Hooks.jsx
@@ -1,7 +1,56 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
+// state full component / class component
+// class Hooks extends React.Component {
+//     state = {
+//         count: 0
+//     }
+
+//     onUpdate = () => {
+//         this.setState({
+//             count: this.state.count + 1
+//         })
+//     }
+
+//     // LIFECYCLE STATEFULL COMPONENT 
+//     // diesksekui ketika render sudah selesai
+//     componentDidMount() {
+//         document.title = `Title Change - ${this.state.count}`
+//     }
+//     // dieksekusi ketika terjadi perubahan pada render
+//     componentDidUpdate() {
+//         document.title = `Title Change - ${this.state.count}`
+//     }
+
+//     // dieksekusi ketika component akan di hapus
+//     componentWillUnmount(){
+//         document.title = "React App"
+//     }
+
+//     render () {
+//         return (
+//         <div > 
+//             <p>Angka Update: {this.state.count}</p>
+//             <button onClick={this.onUpdate}>Update nilai</button>
+//         </div>  
+//         )
+//     }
+    
+// }
+
+// state less componnet / functional component
 const Hooks = () => {
     const [count, setCount] = useState(0);
+
+    // dari LIFECYCLE tadi dalam functional component. kita dapat menggunakan useEffect
+    useEffect(() => {
+        // lifecycle componentDidMount dan ComponentDidUpdate akan bisa dilakukan dengan mudah seperti ini
+        document.title = `Title Change - ${count}`
+        // lifecycle componentWillUnmount dapat dibuat dengan cara ini
+        return () => {
+            document.title = "React App"
+        }
+    });
     return (
         <div > 
             <p>Angka Update: {count}</p>
